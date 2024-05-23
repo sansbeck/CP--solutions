019d7c015f3a68f691b5a28d014c83a7c821ddb5
Sanjeevani Patra <112893195+sansbeck@users.noreply.github.com>
2024-05-23 13:10:25 +0530

    Add files via upload

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define forall(i,st,n) for(int  i=st;i<n;i++)
+#define ull unsigned long long 
+#define ld long double 
+#define mod 1000000007
+#define endl '\n' 
+#define inp(n) int n;cin >> n;
+using namespace std; 
+
+void solve() {
+    int n,k;cin >> n >> k;
+    int arr[n];
+    forall(i,0,n)cin >> arr[i];
+    string ans= (k>1||is_sorted(arr,arr+n))?"YES":"NO";
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int _t; cin >> _t;
+while(_t--)
+        solve();
+    return 0;
+}
